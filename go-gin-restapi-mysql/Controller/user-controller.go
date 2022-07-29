259e392db30a700e96e9c28e116fd8a6448198c7
rahulhuse <rahulhuse92@gmail.com>
2022-07-29 17:08:20 +0530

    go gin restapi using MySql

---
+++ go-gin-restapi-mysql/Controller/user-controller.go
@@ -0,0 +1,77 @@
+package Controller
+
+import (
+	"fmt"
+	"golang-workspace/src/go-gin-restapi-mysql/Models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetUsers(c *gin.Context) {
+	var user []Models.User
+	err := Models.GetAllUsers(&user)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, user)
+	}
+
+}
+
+func CreateUser(c *gin.Context) {
+	var user Models.User
+	c.BindJSON(&user)
+	err := Models.CreateUser(&user)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, gin.H{"message": "New user created"})
+	}
+
+}
+
+func GetUserByID(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var user Models.User
+	err := Models.GetUserByID(&user, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+
+	} else {
+		c.JSON(http.StatusOK, user)
+	}
+}
+
+func UpdateUser(c *gin.Context) {
+
+	var user Models.User
+	id := c.Params.ByName("id")
+	err := Models.GetUserByID(&user, id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, user)
+	}
+	c.BindJSON(&user)
+	err = Models.UpdateUser(&user, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, user)
+	}
+
+}
+
+func DeleteUser(c *gin.Context) {
+
+	var user Models.User
+	id := c.Params.ByName("id")
+	err := Models.DeleteUser(&user, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+
+	} else {
+		c.JSON(http.StatusOK, gin.H{"id" + id: "is deleted"})
+	}
+
+}
