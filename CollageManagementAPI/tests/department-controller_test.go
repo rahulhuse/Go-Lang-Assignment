690912df5b0396c4619d40c8b4fd3ff0ba2ca14f
rahulhuse <rahulhuse92@gmail.com>
2022-08-10 12:52:29 +0530

    test cases added for department controller

--- CollageManagementAPI/tests/department-controller_test.go
+++ CollageManagementAPI/tests/department-controller_test.go
@@ -1,63 +1,117 @@
-package controllers
+package tests
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
 	"gorm-test/controllers"
+	"gorm-test/models"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetAllDepartments(t *testing.T) {
-	type args struct {
-		c *gin.Context
+	r := gin.Default()
+	r.GET("/collage-api/department", controllers.GetAllDepartments)
+	req, err := http.NewRequest("GET", "/collage-api/department", nil)
+
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
 	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			controllers.GetAllDepartments(tt.args.c)
-		})
-	}
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	var dept []models.Department
+
+	json.Unmarshal(w.Body.Bytes(), &dept)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.NotEmpty(t, dept)
+
 }
 
 func TestCreateDepartment(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-
-	// Setup your router, just like you did in your main function, and
-	// register your routes
 	r := gin.Default()
 	r.POST("/collage-api/department", controllers.CreateDepartment)
-
-	// Create the mock request you'd like to test. Make sure the second argument
-	// here is the same as one of the routes you defined in the router setup
-	// block!
-	req, err := http.NewRequest(http.MethodPost, "/collage-api/department", nil)
-
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	department := models.Department{
+		DepartmentID:   5,
+		DepartmentName: "IT",
+		CollageID:      1,
+	}
+	jsonValue, _ := json.Marshal(department)
+	req, err := http.NewRequest("POST", "/collage-api/department", bytes.NewBuffer(jsonValue))
 
 	if err != nil {
 		t.Fatalf("Couldn't create request: %v\n", err)
 	}
 
-	// Create a response recorder so you can inspect the response
 	w := httptest.NewRecorder()
-
-	// Perform the request
 	r.ServeHTTP(w, req)
-	fmt.Println(w.Body)
+	assert.Equal(t, http.StatusCreated, w.Code)
 
-	// Check to see if the response was what you expected
-	if w.Code == http.StatusCreated {
-		t.Logf("Expected to get status %d is same ast %d\n", http.StatusCreated, w.Code)
-	} else {
-		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusCreated, w.Code)
+}
+
+func TestGetDepartmentByID(t *testing.T) {
+
+	r := gin.Default()
+	r.GET("/collage-api/department/1", controllers.GetAllDepartments)
+	req, err := http.NewRequest("GET", "/collage-api/department/1", nil)
+
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
 	}
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+}
+
+func TestUpadateDepartment(t *testing.T) {
+
+	r := gin.Default()
+
+	r.PUT("/collage-api/department/:id", func(c *gin.Context) {
+		id := c.Param("")
+		c.String(http.StatusOK, "Hello %s", id)
+	}, controllers.UpadateDepartment)
+
+	department := models.Department{
+		DepartmentID:   5,
+		DepartmentName: "CIVIL",
+		CollageID:      1,
+	}
+	jsonValue, _ := json.Marshal(department)
+	reqFound, err := http.NewRequest("PUT", "/collage-api/department/5", bytes.NewBuffer(jsonValue))
+
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
+	}
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, reqFound)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// reqNotFound, _ := http.NewRequest("PUT", "/collage-api/department/13", bytes.NewBuffer(jsonValue))
+	// w = httptest.NewRecorder()
+	// r.ServeHTTP(w, reqNotFound)
+	// assert.Equal(t, http.StatusNotFound, w.Code)
+
+}
+
+func TestDeleteDepartmentByID(t *testing.T) {
+
+	req, err := http.NewRequest(http.MethodDelete, "/collage-api/department/3", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	r := gin.Default()
+	r.DELETE("/collage-api/department/:collageid", controllers.DeleteCollageCon)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
 
 }
